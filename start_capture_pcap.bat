f66e577cfffa58698ee6d1ae2321e6e86fbec7c5
Alain Bouchard <abouchar@matrox.com>
2025-12-17 09:18:37 -0500

    use a frame metric for video instead of packets

--- start_capture_pcap.bat
+++ start_capture_pcap.bat
@@ -12,11 +12,11 @@ if "%IPMX_VENDOR_PCAP_CAPTURE%" == "LOCAL" (
 @REM for VB440 capture
 if "%IPMX_VENDOR_PCAP_CAPTURE%" == "VB440" (
     if "%4" == "video" (
-	    curl.exe -k "https://192.168.112.57/probe/api/captures/?responseMode=pcap&deleteOnComplete" -H "Content-Type: application/json" -d "{\"receiverIds\":[\"e64f24f4-e584-5d4a-9d26-0507dde17e65\"],\"packetLimit\":6000,\"timeLimit\":5000,\"captureRTCP\":true}" -o "%1" 2>&1
+	    curl.exe -k "https://10.20.10.249/probe/api/captures/?responseMode=pcap&deleteOnComplete" -H "Content-Type: application/json" -d "{\"receiverIds\":[\"e64f24f4-e584-5d4a-9d26-0507dde17e65\"],\"frameLimit\":30,\"timeLimit\":5000,\"captureRTCP\":true}" -o "%1" 2>&1
 	    if errorlevel 1 echo ERROR: curl command failed for video capture
     )
     if "%4" == "audio" (
-    	curl.exe -k "https://192.168.112.57/probe/api/captures/?responseMode=pcap&deleteOnComplete" -H "Content-Type: application/json" -d "{\"receiverIds\":[\"ea47ad4c-6c49-5b87-a23c-6ab8391f5b87\"],\"packetLimit\":1000,\"timeLimit\":5000,\"captureRTCP\":true}" -o "%1" 2>&1
+    	curl.exe -k "https://10.20.10.249/probe/api/captures/?responseMode=pcap&deleteOnComplete" -H "Content-Type: application/json" -d "{\"receiverIds\":[\"ea47ad4c-6c49-5b87-a23c-6ab8391f5b87\"],\"packetLimit\":1000,\"timeLimit\":5000,\"captureRTCP\":true}" -o "%1" 2>&1
     	if errorlevel 1 echo ERROR: curl command failed for audio capture
     )
 )
